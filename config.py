ffd10ba0bcb62b847a942f96cd26da889c5e258a
JoeMsann <18joe.msane18@gmail.com>
2025-10-03 11:53:45 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from gradio_client import Client
+
+class Config:
+    """
+    Configuration class to hold API keys and other parameters.
+    """
+    def __init__(self):
+        """
+        Initializes the configuration by loading environment variables and LLM models.
+        """
+        load_dotenv()
+        self.groq_api_key: str = os.getenv("GROQ_API_KEY")
+        self.langsmith_api_key: str = os.getenv("LANGSMITH_API_KEY")
+        self.hf_token: str = os.getenv("HF_TOKEN")
+        
+        # Initialize models
+        # Exoplanet Detection Model
+        self.exoplanet_detection_model = Client("chadiawar977/Nasa_space" , self.hf_token)
+
+        # Kimi K2 Instruct
+        self.conversation_model = ChatGroq(
+            model="moonshotai/kimi-k2-instruct-0905",
+            temperature=1,
+            api_key=self.groq_api_key
+        )
+        # GPT OSS 120B
+        self.reasoning_model = ChatGroq(
+            model="openai/gpt-oss-120b",
+            temperature=0,
+            api_key=self.groq_api_key
+        )
+app_config = Config()
